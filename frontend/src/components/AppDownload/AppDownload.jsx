e77c0bd96e194a5ecda589872400884103eece0e
VictorG10 <victorgbadebo10@gmail.com>
2024-03-26 13:22:36 +0100

    initial commits

---
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./appdownload.css";
+
+const AppDownload = () => {
+  return (
+    <div className="app-download" id="app-download">
+      AppDownload
+    </div>
+  );
+};
+
+export default AppDownload;
+
+// 2:00:11
